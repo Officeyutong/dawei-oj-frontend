664e11ad1dd8eabd314590282e65996924ea353a
Clairjoestar <1144950842@qq.com>
2024-09-29 22:15:51 +0800

    1

--- src/app/views/visual_programming/UserSubmissionListModal.tsx
+++ src/app/views/visual_programming/UserSubmissionListModal.tsx
@@ -30,7 +30,12 @@ const UserSubmissionListModal: React.FC<{ uid: number; homeworkId: number; close
         })();
     }, [data, homeworkId, uid]);
     const handleShareURL = (submissionId: Number) => {
-        const url = `${window.location.host}/scratch?oj_submission_id=${submissionId}`;
+        let url;
+        if (window.location.port === ' ') {
+            url = `${window.location.protocol}//${window.location.host}/scratch?oj_submission_id=${submissionId}`;
+        } else {
+            url = `${window.location.protocol}//${window.location.host}:${window.location.port}/scratch?oj_submission_id=${submissionId}`;
+        }
         console.log(url)
         clipboard.write(url);
         showSuccessPopup("样例内容已复制到剪贴板", 700);
