a329b45713ff7ecdee2289622d2adfdc04f264e2
clair joestar <115263660+Clairjoestar@users.noreply.github.com>
2024-09-30 17:44:43 +0900

    分享连接功能 (#8)
    * qaq
    
    * 1
    
    * q
    
    * 1

--- src/app/views/visual_programming/UserSubmissionListModal.tsx
+++ src/app/views/visual_programming/UserSubmissionListModal.tsx
@@ -1,9 +1,11 @@
-import { Button, Dimmer, Loader, Modal, Table } from "semantic-ui-react";
+import { Button, Dimmer, Icon, Loader, Modal, Table } from "semantic-ui-react";
 import { HomeworkDetail, HomeworkSubmissionListEntry } from "./client/types";
 import { useEffect, useState } from "react";
 import visualProgrammingClient from "./client/VisualProgrammingClient";
 import { timeStampToString } from "../../common/Utils";
 import SubmissionDetailedModal, { BasicSubmissionDetailProps } from "./management/SubmissionDetailedModal";
+import * as clipboard from "clipboardy";
+import { showSuccessPopup } from "../../dialogs/Utils";
 
 const UserSubmissionListModal: React.FC<{ uid: number; homeworkId: number; closeCallback: () => void; }> = ({ uid, homeworkId, closeCallback }) => {
     const [loading, setLoading] = useState(false);
@@ -20,11 +22,18 @@ const UserSubmissionListModal: React.FC<{ uid: number; homeworkId: number; close
                 setData(a.data);
                 setHomeworkData(b);
 
+
             } catch { } finally {
+
                 setLoading(false);
             }
         })();
     }, [data, homeworkId, uid]);
+    const handleShareURL = (submissionId: Number) => {
+        const url = `${window.location.protocol}//${window.location.host}/scratch?oj_submission_id=${submissionId}`;
+        clipboard.write(url);
+        showSuccessPopup("样例内容已复制到剪贴板", 700);
+    }
     return <>
         {selectedSubmission !== null && <SubmissionDetailedModal
             {...selectedSubmission}
@@ -45,6 +54,7 @@ const UserSubmissionListModal: React.FC<{ uid: number; homeworkId: number; close
                             <Table.HeaderCell>提交时间</Table.HeaderCell>
                             <Table.HeaderCell>是否已点评</Table.HeaderCell>
                             <Table.HeaderCell>操作</Table.HeaderCell>
+                            <Table.HeaderCell>分享连接</Table.HeaderCell>
                         </Table.Row>
                     </Table.Header>
                     <Table.Body>
@@ -55,6 +65,12 @@ const UserSubmissionListModal: React.FC<{ uid: number; homeworkId: number; close
                             </Table.Cell>
                             <Table.Cell>
                                 <Button color="green" size="small" onClick={() => setSelectedSubmission({ homeworkId: item.homework_id, submissionId: item.submission_id, uid: item.user.uid })}>查看详情</Button>
+                            </Table.Cell>
+                            <Table.Cell>
+                                <Button color="green" size="small" onClick={() => handleShareURL(item.submission_id)
+                                }>
+                                    <Icon name='share square' />
+                                </Button>
                             </Table.Cell>
                         </Table.Row>)}
                     </Table.Body>
