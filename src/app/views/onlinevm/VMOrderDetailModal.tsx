7748a994699691c9419088b4adec9de1c08f3b30
Officeyutong <yt.xyxx@gmail.com>
2024-10-04 00:41:39 +0800

    update

---
+++ src/app/views/onlinevm/VMOrderDetailModal.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import { Button, Dimmer, Form, Loader, Modal } from "semantic-ui-react";
+import { OnlineVMOrderEntry } from "./client/types";
+import onlineVMClient, { translateVMOrderStatus } from "./client/OnlineVMClient";
+import { ComplexUserLabel, timeStampToString } from "../../common/Utils";
+import ChargeSchemaList from "./utils/ChargeSchemaList";
+import AceEditor from "react-ace";
+import { useAceTheme } from "../../states/StateUtils";
+
+const VMOrderDetailModal: React.FC<{ uid?: number; orderId: number; onClose: () => void }> = ({ uid, orderId, onClose }) => {
+    const aceTheme = useAceTheme();
+    const [loading, setLoading] = useState(false);
+    const [loaded, setLoaded] = useState(false);
+    const [data, setData] = useState<OnlineVMOrderEntry | null>(null);
+    useEffect(() => {
+        if (!loaded) (async () => {
+            try {
+                setLoading(true);
+                setData((await onlineVMClient.getVMOrderList(1, uid, [orderId])).data[0]);
+                setLoaded(true);
+            } catch { } finally {
+                setLoading(false);
+            }
+        })();
+    }, [loaded, orderId, uid])
+    return <Modal open size="small">
+        <Modal.Header>查看虚拟机订单详情</Modal.Header>
+        <Modal.Content>
+            {loading && <Dimmer active><Loader></Loader></Dimmer>}
+            {loaded && data !== null && <Form>
+
+                <Form.Group widths={4}>
+                    <Form.Field>
+                        <label>订单编号</label>
+                        {data.order_id}
+                    </Form.Field>
+                    <Form.Field>
+                        <label>订单用户</label>
+                        <ComplexUserLabel user={data.user}></ComplexUserLabel>
+                    </Form.Field>
+                    <Form.Field>
+                        <label>订单状态</label>
+                        {translateVMOrderStatus(data.status)}
+                    </Form.Field>
+                    <Form.Field>
+                        <label>产品名</label>
+                        {data.product.name}
+                    </Form.Field>
+
+                </Form.Group>
+                <Form.Group widths={3}>
+                    <Form.Field>
+                        <label>创建时间</label>
+                        {timeStampToString(data.create_time)}
+                    </Form.Field>
+                    <Form.Field>
+                        <label>最后更新时间</label>
+                        {timeStampToString(data.last_update_time)}
+                    </Form.Field>
+                    <Form.Field>
+                    <label>腾讯云实例ID</label>
+                    {data.tencent_cloud_id}
+                </Form.Field>
+                </Form.Group>
+                <Form.Field>
+                        <label>收费模式</label>
+                        <ChargeSchemaList data={data.charge_schema}></ChargeSchemaList>
+                    </Form.Field>
+        
+                {data.admin_description && <Form.Field>
+                    <label>管理员备注</label>
+                    <AceEditor
+                        value={data.admin_description}
+                        name="admin_desc"
+                        mode="plain_text"
+                        width="100%"
+                        height="300px"
+                        wrapEnabled
+                        theme={aceTheme}
+                    ></AceEditor>
+                </Form.Field>}
+            </Form>}
+        </Modal.Content>
+        <Modal.Actions>
+            <Button size="small" color="red" onClick={() => onClose()} disabled={loading}>关闭</Button>
+        </Modal.Actions>
+    </Modal>
+}
+
+export default VMOrderDetailModal;
--- src/app/views/onlinevm/user/VMOrderList.tsx
+++ src/app/views/onlinevm/user/VMOrderList.tsx
@@ -1,18 +1,87 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import CreateVMModal from "./CreateVMModal";
-import { Button } from "semantic-ui-react";
+import { Button, Dimmer, Divider, Header, Loader, Pagination, Table } from "semantic-ui-react";
+import { useSelector } from "react-redux";
+import { StateType } from "../../../states/Manager";
+import { OnlineVMOrderEntry } from "../client/types";
+import onlineVMClient, { translateVMOrderStatus } from "../client/OnlineVMClient";
+import { timeStampToString, useNowTime } from "../../../common/Utils";
+import { DateTime } from "luxon";
+import VMOrderDetailModal from "../VMOrderDetailModal";
 
 const VMOrderList: React.FC<{}> = () => {
     const [showCreateVMModal, setShowCreateVMModal] = useState(false);
-    const loadPage = async (page: number) => {
-
-    };
+    const selfUid = useSelector((s: StateType) => s.userState.userData.uid);
+    const initialReqDone = useSelector((s: StateType) => s.userState.initialRequestDone);
+    const [loaded, setLoaded] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState<OnlineVMOrderEntry[]>([]);
+    const [page, setPage] = useState(1);
+    const [pageCount, setPageCount] = useState(0);
+    const [showingOrder, setShowingOrder] = useState<OnlineVMOrderEntry | null>(null);
+    const loadPage = useCallback(async (page: number) => {
+        try {
+            setLoading(true);
+            const resp = await onlineVMClient.getVMOrderList(page, selfUid);
+            setPageCount(resp.pageCount);
+            setPage(page);
+            setData(resp.data);
+            setLoaded(true);
+        } catch { } finally {
+            setLoading(false);
+        }
+    }, [selfUid])
+    useEffect(() => {
+        if (!loaded && initialReqDone) loadPage(1);
+    }, [initialReqDone, loadPage, loaded])
+    const nowTime = useNowTime();
     return <>
+        <Header as="h2">虚拟机订单列表</Header>
+        {showingOrder !== null && <VMOrderDetailModal
+            onClose={() => setShowingOrder(null)}
+            orderId={showingOrder.order_id}
+            uid={selfUid}
+        ></VMOrderDetailModal>}
+        {loading && <Dimmer active><Loader></Loader></Dimmer>}
         {showCreateVMModal && <CreateVMModal onClose={flag => {
             if (flag) loadPage(1);
             setShowCreateVMModal(false);
         }}></CreateVMModal>}
         <Button color="green" onClick={() => setShowCreateVMModal(true)}>创建虚拟机</Button>
+        <Divider></Divider>
+        {loaded && <>
+            <Table>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>订单编号</Table.HeaderCell>
+                        <Table.HeaderCell>创建时间</Table.HeaderCell>
+                        <Table.HeaderCell>商品名</Table.HeaderCell>
+                        <Table.HeaderCell>状态</Table.HeaderCell>
+                        <Table.HeaderCell>已计费时长</Table.HeaderCell>
+                        <Table.HeaderCell>操作</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {data.map(item => <Table.Row key={item.order_id}>
+                        <Table.Cell>{item.order_id}</Table.Cell>
+                        <Table.Cell>{timeStampToString(item.create_time)}</Table.Cell>
+                        <Table.Cell>{item.product.name}</Table.Cell>
+                        <Table.Cell negative={item.status === "error"}>{translateVMOrderStatus(item.status)}</Table.Cell>
+                        <Table.Cell>{Math.ceil(nowTime.diff(DateTime.fromSeconds(item.create_time)).as("seconds") / 3600)}小时</Table.Cell>
+                        <Table.Cell>
+                            <Button size="small" onClick={() => setShowingOrder(item)}>查看详情</Button>
+                        </Table.Cell>
+                    </Table.Row>)}
+                </Table.Body>
+            </Table>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <Pagination
+                    totalPages={Math.max(1, pageCount)}
+                    activePage={page}
+                    onPageChange={(_, d) => loadPage(d.activePage as number)}
+                ></Pagination>
+            </div>
+        </>}
     </>
 };
 
